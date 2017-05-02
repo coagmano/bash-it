db6faff0473777834b8f2c71e4c597c24400d387
Frederick Stark <coagmano@gmail.com>
2017-05-02 10:51:51 +1000

    remove broken sierra sshkeys plugin

--- plugins/available/osx-sshkeys.plugin.bash
+++
@@ -1,5 +0,0 @@
-# Force Sierra to load ssh key passphrases on new session
-cite about-plugin
-about-plugin 'Force Sierra to load ssh key passphrases in new sessions'
-
-{ eval `ssh-agent`; ssh-add -A; } &>/dev/null
