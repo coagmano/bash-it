0b9c267e6d4baa4853c9060d0683b2f345b44878
Fred Stark <fred@displayground.com.au>
2019-02-14 12:06:53 +1100

    Add more meteor aliases

--- custom/meteor.aliases.bash
+++ custom/meteor.aliases.bash
@@ -4,6 +4,7 @@ about-alias 'common meteor abbreviations'
 # Aliases
 alias m='meteor'
 alias ms='meteor --settings'
+alias mss='meteor --settings settings.json'
 alias mn='meteor npm'
 alias mni='meteor npm install'
 alias mns='meteor npm start'
